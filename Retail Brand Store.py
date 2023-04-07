a12565d4e545e6939c8e1ba90f2cfcb36b49edf8
Yash Mittal <102669147+yashm02@users.noreply.github.com>
2023-04-07 18:36:49 +0530

    Update Retail Brand Store.py

--- Retail Brand Store.py
+++ Retail Brand Store.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from geopy.geocoders import ArcGIS
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from csv import writer
 
 from webdriver_manager.chrome import ChromeDriverManager
@@ -12,40 +16,51 @@ options=webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_experimental_option('excludeSwitches',['enable-logging'])
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
-url="https://stores.titan.co.in/titan-world-watch-shop-lajpat-nagar-new-delhi-187114/Home?utm_source=locator&utm_medium=googleplaces"
 
-driver.get(url)
-
-time.sleep(2)
-
-soup=BeautifulSoup(driver.page_source,'lxml')
 
 with open('Retail_Brand_Store_Details.csv','w', encoding='utf8', newline='') as f:
     thewriter=writer(f)
     header=['Store Name', 'Address', 'Timings', 'Coordinates', 'Phone Number']
     thewriter.writerow(header)
-    store_name=soup.find("h1",id="speakableIntro").text.strip()
-    # print("Store Name -",store_name.text.strip())
-    address1=soup.select('div.info-text>span')
-    # print(address1)
-    address=""
-    for item in address1:
-        address+=item.text+", "
-    timings=soup.find('span',class_='info-text').text
-    # print("Timings -",timings.text)
-    location=geolocator.geocode(address)
-    Coordinates=(location.latitude,location.longitude)
-    # print("Address -",address)
-    # print((location.latitude,location.longitude))
-    
-    mobile=soup.select('div.info-text>a')
-    phone=''
-    for item in mobile:
-        phone=item.text.strip()
-    phone+='\t'    
-    # print(phone)
-    info=[store_name, address, timings, Coordinates, phone]
-    thewriter.writerow(info)
+    for i in range(1,99):
+        url="https://stores.titan.co.in/?page="+str(i)
+        driver.get(url)
+
+        time.sleep(2)
+
+        soup=BeautifulSoup(driver.page_source,'lxml')
+        outlet_lists=soup.find_all("div",class_="outlet-list")
+
+
+        for outlet_list in outlet_lists:
+            store_info=outlet_list.select("div.store-info-box")
+            for stores in store_info:
+                store1=stores.select('li.outlet-name>div.info-text')
+                store=''
+                for store_name in store1:
+                    store+=store_name.text.strip()
+                address1=stores.select('li.outlet-address>div.info-text')
+
+                address=''
+                for item in address1:
+                    address+=item.text.strip()
+                    address+=', '
+                timing1=stores.select('li.outlet-timings>div.info-text')
+                timing=''
+                for t in timing1:
+                    timing+=t.text.strip()
+                location=geolocator.geocode(address)
+                try:
+                    Coordinates=(location.latitude,location.longitude)
+                except:
+                    Coordinates="Not Found"
+                phone1=stores.select('li.outlet-phone>div.info-text')
+                phone=''
+                for item in phone1:
+                    phone+=item.text.strip()
+                phone+='\t'
+                info=[store, address, timing, Coordinates, phone]
+                thewriter.writerow(info)
   
 
 time.sleep(2)
